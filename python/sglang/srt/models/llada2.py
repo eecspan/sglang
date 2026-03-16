e858e122d6f300c90350f4231e6f59ff53e00ae5
panyudong.pyd <panyudong.pyd@antgroup.com>
2026-03-16 21:15:45 +0800

    add support for more models.

--- python/sglang/srt/models/llada2.py
+++ python/sglang/srt/models/llada2.py
@@ -501,6 +501,7 @@ class LLaDA2MoeAttention(nn.Module):
             num_kv_heads=self.num_kv_heads,
             layer_id=layer_id,
             attn_type=AttentionType.ENCODER_ONLY,
+            quant_config=quant_config,
             prefix=add_prefix("attn", prefix),
         )
 
